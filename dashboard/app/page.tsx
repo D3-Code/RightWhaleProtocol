9e50aab34a92496c821283e9b6ed49b378c42a62
Geraldo Diniz <dthreecode@gmail.com>
2026-01-17 21:38:34 -0500

    feat(dashboard): update git url to D3-Code

--- dashboard/app/page.tsx
+++ dashboard/app/page.tsx
@@ -196,7 +196,7 @@ export default function Home() {
                   <Book className="w-5 h-5 group-hover:scale-110 transition-transform" />
                   <span className="text-xs font-bold uppercase tracking-wider">Documentation</span>
                 </Link>
-                <a href="https://github.com/geraldodiniz/RightWhaleProtocol" target="_blank" className="bg-white/5 hover:bg-white/10 border border-white/10 hover:border-white/20 text-zinc-400 hover:text-white py-3 rounded transition-all flex items-center justify-center gap-2 group">
+                <a href="https://github.com/D3-Code/RightWhaleProtocol" target="_blank" className="bg-white/5 hover:bg-white/10 border border-white/10 hover:border-white/20 text-zinc-400 hover:text-white py-3 rounded transition-all flex items-center justify-center gap-2 group">
                   <Github className="w-5 h-5 group-hover:scale-110 transition-transform" />
                   <span className="text-xs font-bold uppercase tracking-wider">Git</span>
                 </a>
